bc8aa8816cf3ee3ca942e95f6ac475c7f8d65b31
Wesley-yuri <wesley_analista@outlook.com>
2023-02-18 00:34:58 -0300

    Entendendo M elhor atributos e metodos

---
+++ tipos-variaveis/src/Operador.java
@@ -0,0 +1,14 @@
+public class Operador {
+
+    public static void main(String[] args) {
+
+        
+       int resultado = 0;
+
+       System.out.print(resultado >= 10 ? true : false);
+                        
+    
+    
+    }
+
+}
